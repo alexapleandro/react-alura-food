946abfe78da0f308f72c7a7c072371c872d106e5
alex <alex@ingadigital.com.br>
2022-06-16 13:46:12 -0300

    caregamento dos dados da lista atrvé de um arquivo estático

--- src/pages/Cardapio/itens/item/index.tsx
+++ src/pages/Cardapio/itens/item/index.tsx
@@ -1,29 +1,38 @@
 import styles from './Item.module.scss';
-import logo from  'assets/logo.svg';
+import cardapio from '../itens.json';
+import classNames from 'classnames';
 
-export default function Item(){
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props){
+
+    const {title, description, category, size, serving, price, photo} = props;
+
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt="imagem" />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2>Macarrão</h2>
-                    <p>Descrição do macarraõ</p>
+                    <h2>{ title }</h2>
+                    <p>{ description }</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
-                        Massa
+                    <div className={classNames({
+                        [styles.item__tipo] : true,
+                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
+                    })}>
+                        { category.label }
                     </div>
                     <div className={styles.item__porcao}>
-                        400g
+                        { size }g
                     </div>
                     <div className={styles.item__qtdpessoas}>
-                        Serve 2 Pessoas
+                        Serve {serving} Pessoa{serving === 1 ? "" : "s"}
                     </div>
                     <div className={styles.item__valor}>
-                        R$ 50
+                        R$ { price.toFixed(2) }
                     </div>
                 </div>
             </div>
