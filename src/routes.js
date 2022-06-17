015984cfd1526298e72428aefcaa814e9bfd6d7b
alex <alex@ingadigital.com.br>
2022-06-17 13:57:26 -0300

    Refatorando o temas do titulo e do container

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "pages/Inicio";
 import Cardapio from "pages/Cardapio";
 import Menu from "components/Menu";
+import PaginaPadrao from "components/PaginaPadrao";
 
 export default function AppRouter() {
 	return (
-		<Router basename="/react-alura-food">
-			<Menu />
-			<Routes>
-				<Route path="/" element={<Inicio />}/>
-				<Route path="/cardapio" element={<Cardapio />}/>
-			</Routes>
-		</Router>
+		<main>
+			<Router basename="/react-alura-food">
+				<Menu />
+				<Routes>
+					<Route path="/" element={<PaginaPadrao />} >
+						<Route index element={<Inicio />} />
+						<Route path="cardapio" element={<Cardapio />} />
+					</Route>
+				</Routes>
+			</Router>
+		</main>
 	);
 }
