611669aa973bc46a4e6e160fb2b5393a52e107b1
alex <alex@ingadigital.com.br>
2022-06-17 11:15:22 -0300

    alteração da configuração do eslint

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,15 +1,15 @@
-import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
-import { useState } from 'react';
-import Buscador from './buscador';
-import Filtros from './filtros';
-import Ordenador from './ordenador';
-import Itens from './itens';
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import { useState } from "react";
+import Buscador from "./buscador";
+import Filtros from "./filtros";
+import Ordenador from "./ordenador";
+import Itens from "./itens";
 
 export default function Cardapio() {
-	const [busca, setBusca] = useState('');
+	const [busca, setBusca] = useState("");
 	const [filtro, setFiltro] = useState<number | null>(null);
-	const [ordenador, setOrdenador] = useState('');
+	const [ordenador, setOrdenador] = useState("");
 	return (
 		<main>
 			<nav className={styles.menu}>
--- src/pages/Cardapio/itens/item/index.tsx
+++ src/pages/Cardapio/itens/item/index.tsx
@@ -1,6 +1,6 @@
-import styles from './Item.module.scss';
-import cardapio from '../itens.json';
-import classNames from 'classnames';
+import styles from "./Item.module.scss";
+import cardapio from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof cardapio[0];
 
@@ -29,7 +29,7 @@ export default function Item(props: Props){
 						{ size }g
 					</div>
 					<div className={styles.item__qtdpessoas}>
-                        Serve {serving} Pessoa{serving === 1 ? '' : 's'}
+                        Serve {serving} Pessoa{serving === 1 ? "" : "s"}
 					</div>
 					<div className={styles.item__valor}>
                         R$ { price.toFixed(2) }
